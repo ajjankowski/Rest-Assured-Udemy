37489ae3711dc8d3fb3165ade41b079acca942f0
ajjankowski <aj.jankowski92@gmail.com>
2022-11-03 11:05:03 +0100

    First get and add

--- src/test/java/TestRestAssured.java
+++ src/test/java/TestRestAssured.java
@@ -1,6 +1,35 @@
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
 public class TestRestAssured {
 
-    public static void main(String[] args) {
+    @Test
+    public void getPosts() {
+        // given = preconditions
+        // when  = action
+        // then  = expected result
+
+        // body = logs body
+        // all  = logs body and parameters
+        when().get("https://swapi.dev/api/").then().log().all();
+
+        // expects to have status code xxx -> if other, logs error
+        when().get("https://swapi.dev/api/").then().log().ifValidationFails().statusCode(200);
+    }
+
+    @Test
+    public void addPosts() {
+        String newPost = "{\n" +
+                "\"title\": \"First post\", \n" +
+                "\"author\": \"Name\", \n" +
+                "}";
+
+        given().log().all().contentType(ContentType.JSON).body(newPost)
+                .when().post("https://swapi.dev/api/")
+                .then().log().all();
 
     }
 }
